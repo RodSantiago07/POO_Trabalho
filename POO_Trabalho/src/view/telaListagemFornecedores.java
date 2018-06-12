3e0b75f9dce853521175468a3db138c0b7033227
Fernanda B <fernandabuz@users.noreply.github.com>
2018-06-12 19:13:59 -0300

    telaListagemFornecedores

---
+++ POO_Trabalho/src/view/telaListagemFornecedores.java
@@ -0,0 +1,162 @@
+import java.awt.List;
+import java.util.ArrayList;
+import javax.swing.DefaultListModel;
+import javax.swing.JOptionPane;
+
+public class telaListagemFornecedores extends javax.swing.JFrame {
+
+    public DefaultListModel listModel = new DefaultListModel();
+    ArrayList<clsFornecedor> lista = new ArrayList<clsFornecedor>();
+    
+    public telaListagemFornecedores(ArrayList<clsFornecedor> listaRef) {
+        lista = listaRef;
+        for (int i = 0; i < lista.size(); i++){
+            listModel.addElement(i + " - " + lista.get(i).getNome() + " - " + lista.get(i).getMediaAvaliacoes());
+        }
+        listagem = new javax.swing.JList<>(listModel);
+        initComponents();
+        setLocationRelativeTo(null);
+    }
+
+    private telaListagemFornecedores() {
+        initComponents();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        campoLista = new javax.swing.JScrollPane();
+        listagem = new javax.swing.JList<>();
+        botaoExcluir = new javax.swing.JButton();
+        botaoSair = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Listagem de Fornecedores");
+
+        listagem.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        listagem.setModel(listModel);
+        campoLista.setViewportView(listagem);
+
+        botaoExcluir.setText("Excluir");
+        botaoExcluir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botaoExcluirActionPerformed(evt);
+            }
+        });
+
+        botaoSair.setText("Sair");
+        botaoSair.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botaoSairActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("ID - Fornecedor - Avaliação");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(campoLista, javax.swing.GroupLayout.DEFAULT_SIZE, 525, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(botaoExcluir)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(botaoSair, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(19, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(campoLista, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(botaoExcluir)
+                        .addGap(30, 30, 30))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(botaoSair)
+                        .addContainerGap())))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void botaoSairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoSairActionPerformed
+        // TODO add your handling code here:
+        setVisible(false);
+        // para fechar tudo: System.exit(0);
+    }//GEN-LAST:event_botaoSairActionPerformed
+
+    private void botaoExcluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoExcluirActionPerformed
+        // TODO add your handling code here:
+        try {
+            for (int i = 0; i < lista.size(); i++) {
+                System.out.println("lista -" + lista.get(i).getNome());
+            }
+            int selecionado = listagem.getSelectedIndex();// pega o id do selecionado
+            System.out.println("selecionado - " + selecionado);
+            listModel.removeElementAt(selecionado);// remove a posição baseado no id do selecionado
+            System.out.println("antes - " + lista);
+            lista.remove(selecionado);// remove do arraylist baseado na posição do selecionado
+            System.out.println("depois - " + lista);
+            //limpaLista();
+            telaListagemFornecedores novo = new telaListagemFornecedores(lista);
+        } catch (ArrayIndexOutOfBoundsException err) {
+            JOptionPane.showMessageDialog(rootPane, "Selecione alguma opçao!", "Erro ao remover da lista", JOptionPane.ERROR_MESSAGE);
+        }
+    }//GEN-LAST:event_botaoExcluirActionPerformed
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(telaListagemFornecedores.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(telaListagemFornecedores.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(telaListagemFornecedores.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(telaListagemFornecedores.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new telaListagemFornecedores().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton botaoExcluir;
+    private javax.swing.JButton botaoSair;
+    private javax.swing.JScrollPane campoLista;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JList<String> listagem;
+    // End of variables declaration//GEN-END:variables
+}
