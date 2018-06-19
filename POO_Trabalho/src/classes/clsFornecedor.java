6535125946b0aee00b69ad87529070e72ea7a012
mariaclaudiacb <37425685+mariaclaudiacb@users.noreply.github.com>
2018-06-19 18:49:31 -0300

    Add files via upload

--- POO_Trabalho/src/classes/clsFornecedor.java
+++ POO_Trabalho/src/classes/clsFornecedor.java
@@ -1,60 +1,93 @@
-package classes;
-
+import java.util.*;
 public class clsFornecedor {
-        private int idFornecedor;
+
+	private int idFornecedor;
 	private String nome;
 	private String email;
-	private double mediaAvaliacoes;
-	private int qtdAvaliacoes;
-
-	public void criarProposta(int diasEntrega, int preTotal, int desProposta) {
-            //Desenvolver metodo
+	private Double mediaAvaliacoes;
+	private int qntdAvaliacoes;
+	private String texto;
+	
+	
+	public String getTexto() {
+		return texto;
 	}
 
+	public void setTexto(String texto) {
+		this.texto = " ";
+	}
+
+	clsFornecedor(String nome, String email) {
+		this.nome = nome;
+		this.email = email;
+		this.mediaAvaliacoes = 0.0;
+		this.qntdAvaliacoes = 0;
+		System.out.println("-");
+		
+	}
+	
+	public static clsFornecedor criarFornecedor(String nome, String email) {
+		
+		clsFornecedor fornecedor = new clsFornecedor(nome, email);
+		bancoFornecedor dbFornecedor = bancoFornecedor.getINSTANCE();
+		dbFornecedor.objeto.add(fornecedor);
+		System.out.println(dbFornecedor.objeto.get(dbFornecedor.objeto.size() - 1).getNome());
+		
+		return fornecedor;
+		
+	}
+	public static clsFornecedor adcNovoFornecedor(String nome, String email) {
+		clsFornecedor fornecedor = new clsFornecedor(nome, email);
+		bancoFornecedor dbFornecedor = bancoFornecedor.getINSTANCE();
+		dbFornecedor.objeto.add(fornecedor);
+		System.out.println(dbFornecedor.objeto.get(dbFornecedor.objeto.size() - 1).getNome());
+		
+		return fornecedor;
+	}
+	
+
+	
+//	public void criarProposta(int diasEntrega, int precoTotal, String descProposta) {
+//		clsProposta proposta = new clsProposta();
+//	}
+	
 	public void cancelarProposta(int statusCompra) {
-            //Desenvolver metodo
+		
+	}
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdFornecedor() {
+		return idFornecedor;
 	}
 
-	public String getNome() {
-            return nome;
+	public void setIdFornecedor(int idFornecedor) {
+		this.idFornecedor = idFornecedor;
 	}
 
 	public String getEmail() {
-            return email;
+		return email;
 	}
-
-	public double getMediaAvaliacoes() {
-            return mediaAvaliacoes;
-	}
-
-	public int getQtdAvaliacoes() {
-            return qtdAvaliacoes;
-	}
-
-	public void setNome(String nome) {
-            this.nome = nome;
-	}
-
 	public void setEmail(String email) {
-            this.email = email;
+		this.email = email;
 	}
-
-        public int getIdFornecedor() {
-            return idFornecedor;
-        }
-
-        public void setIdFornecedor(int idFornecedor) {
-            this.idFornecedor = idFornecedor;
-        }
-
-	public void setMediaAvaliacoes(double mediaAvaliacoes) {
-            this.mediaAvaliacoes = mediaAvaliacoes;
+	public Double getMediaAvaliacoes() {
+		return mediaAvaliacoes;
 	}
-
-	public void setQtdAvaliacoes(int qtdAvaliacoes) {
-            this.qtdAvaliacoes = qtdAvaliacoes;
+	public void setMediaAvaliacoes(Double mediaAvaliacoes) {
+		this.mediaAvaliacoes = mediaAvaliacoes;
 	}
-        
-        
-
+	public int getQntdAvaliacoes() {
+		return qntdAvaliacoes;
+	}
+	public void setQntdAvaliacoes(int qntdAvaliacoes) {
+		this.qntdAvaliacoes = qntdAvaliacoes;
+	}
+	
+	
 }
